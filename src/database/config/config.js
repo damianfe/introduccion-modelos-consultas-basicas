6cdb93aac337d51bb0b5f20a9bfe173f26e7b4cb
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-06-13 08:53:51 -0300

    Agrego readme

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,23 +4,23 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABSE,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port : process.env.DB_PORT
+    port: process.env.PORT
   },
-  test: {
+  "test": {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mysql"
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql",
-  },
-};
+    dialect: "mysql"
+  }
+}
